bf21f053a47c481105509fec732b1e2ee04b60a1
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-09-18 21:13:35 +0200

    Add parser

---
+++ src/Moo.hs
@@ -0,0 +1,35 @@
+module Moo
+  ( COWExpression
+  , moos
+  )
+  where
+
+data COWExpression = COWGoBack
+                   | COWMemBack
+                   | COWMemForward
+                   | COWMemExec
+                   | COWStdAscii
+                   | COWMemDec
+                   | COWMemInc
+                   | COWGoForward
+                   | COWMemReset
+                   | COWRegister
+                   | COWStdPrintInt
+                   | COWStdReadInt
+                   deriving (Eq, Show, Enum)
+
+moos :: [String]
+moos = [ "moo"
+       , "mOo"
+       , "moO"
+       , "mOO"
+       , "Moo"
+       , "MOo"
+       , "MoO"
+       , "MOO"
+       , "OOO"
+       , "MMM"
+       , "OOM"
+       , "oom"
+       ]
+ 
---
+++ src/Parser.hs
@@ -0,0 +1,21 @@
+module Parser
+  (cowParser)
+where
+
+import Text.Parsec
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+
+import Moo
+
+mooParser :: Parsec String () COWExpression
+mooParser = choice mooParsers >>= return . toMooExpression
+  where
+    mooParsers = map (try . string) moos :: [Parsec String () String]
+    toMooExpression = toEnum . fromJust . flip elemIndex moos
+
+cowParser :: Parsec String () [COWExpression]
+cowParser = optional notMooParser *> mooParser `sepEndBy` notMooParser
+  where
+    notMooParser = manyTill anyChar (eof <|>
+                                      (lookAhead mooParser *> return ()))
