140b342bf99f7f640202c99450617e7e972b2a45
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-09-29 13:45:21 +0200

    Comment Fibonacci test not terminating

--- test/Spec.hs
+++ test/Spec.hs
@@ -25,4 +25,4 @@ helloWorld2 :: TestCase
 helloWorld2 = ("Hello, World with loops and register", "MoO MoO MoO MoO MoO MoO MoO MoO MOO moO MoO MoO MoO MoO MoO moO MoO MoO MoO MoO moO MoO MoO MoO MoO moO MoO MoO MoO MoO MoO MoO MoOMoO MoO moO MoO MoO MoO MoO mOo mOo mOo mOo mOo MOo moo moO moO moO moO Moo moO MOO mOo MoO moO MOo moo mOo MOo MOo MOo Moo MoO MoO MoO MoO MoO MoO MoO Moo Moo MoO MoO MoO Moo MMM mOo mOo mOo MoO MoO MoO MoO Moo moO Moo MOO moO moO MOo mOo mOo MOo moo moO moO MoO MoO MoO MoO MoO MoO MoO MoO Moo MMM MMM Moo MoO MoO MoO Moo MMM MOo MOo MOo Moo MOo MOo MOo MOo MOo MOo MOo MOo Moo mOo MoO Moo")
 
 fibonacci :: TestCase
-fibonacci = ("Fibonacci with nested loops", "MoO moO MoO mOo MOO OOM MMM moO moOMMM mOo mOo moO MMM mOo MMM moO moOMOO MOo mOo MoO moO moo mOo mOo moo")
+fibonacci = ("Fibonacci with nested loops !DOESN'T TERMINATE!", "MoO moO MoO mOo MOO OOM MMM moO moOMMM mOo mOo moO MMM mOo MMM moO moOMOO MOo mOo MoO moO moo mOo mOo moo")
